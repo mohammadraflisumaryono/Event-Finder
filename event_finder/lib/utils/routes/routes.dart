91bceed33e0b5e7d7a361de12178a65dad44c7c6
jasmeinalbr <jasmeinalbaar29@gmail.com>
2024-12-19 21:14:05 +0700

    routes

---
+++ event_finder/lib/utils/routes/routes.dart
@@ -0,0 +1,14 @@
+
+import 'package:event_finder/utils/routes/routes_name.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+
+  // static Routes<dynamic> generateRoute(RouteSettings settings) {
+
+  //   switch(settings.name) {
+  //     case RoutesName.home:
+  //       return MaterialPageRoute(builder: (BuildContext context) => const );
+  //   }
+  // }
+}
---
+++ event_finder/lib/utils/routes/routes_name.dart
@@ -0,0 +1,12 @@
+ 
+ class RoutesName {
+
+  // login route name
+  static const String login = 'login_screen';
+
+  // register route name
+  static const String register = 'register_screen';
+
+  // homescreen route name
+  static const String home = 'home_screen';
+ }
