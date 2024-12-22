fc05831ef004b8ac7198512316343f064ca1c2ae
Imany Fauzy Rahman <imanyfauzyr123@gmail.com>
2024-12-22 18:58:10 +0700

    benerin create event

---
+++ event_finder/lib/view_model/create_account_page_model.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: unused_import, undefined_shown_name, override_on_non_overriding_member
+
+import 'package:event_finder/view/create_account_page_widget.dart';
+import 'package:event_finder/view_model/home_page_model.dart';
+
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '../view_model/create_account_page_model.dart'
+    show CreateAccountPageWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+mixin CreateAccountPageModel implements FlutterFlowModel {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode1;
+  TextEditingController? emailTextController;
+  String? Function(BuildContext, String?)? emailTextControllerValidator;
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode2;
+  TextEditingController? passwordTextController;
+  late bool passwordVisibility;
+  String? Function(BuildContext, String?)? passwordTextControllerValidator;
+
+  @override
+  Future<void> initState(BuildContext context) async {
+    passwordVisibility = false;
+  }
+
+  @override
+  Future<void> dispose() async {
+    textFieldFocusNode1?.dispose();
+    emailTextController?.dispose();
+
+    textFieldFocusNode2?.dispose();
+    passwordTextController?.dispose();
+  }
+}
