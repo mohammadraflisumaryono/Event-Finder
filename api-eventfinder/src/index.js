2868a304fe4ffdefccc0b1fe7416742dcb1c2ca0
mohammadraflisumaryono <mohammadraflisumaryono@gmail.com>
2024-12-26 15:30:51 +0700

    Update initial route to home and fix image upload path; add static file serving for uploads

--- api-eventfinder/src/index.js
+++ api-eventfinder/src/index.js
@@ -2,6 +2,7 @@
 const app = require('./app');
 const express = require('express');
 const connectToDb = require('./config/db');
+const path = require('path');
 const UserModel = require('./models/UserModel');
 
 require('dotenv').config();
@@ -13,6 +14,8 @@ require('./config/cronjob');
 app.listen(port, () => {
     console.log(`Server running on port http://localhost:${port}`);
 });
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.use((err, req, res, next) => {
     res.status(400).json({
